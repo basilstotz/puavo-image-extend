155b33c885dceace970becec014e335a8125603f
Basil Stotz <stotz.basil@opinsys-q0AXi4JA>
2022-02-03 16:59:38 +0100

    *

--- config/parts.d/puavo-conf/install.sh
+++ config/parts.d/puavo-conf/install.sh
@@ -6,12 +6,12 @@ cd $(dirname $0)
 cp scripts/dot.preinit /etc/puavo-conf/scripts/.preinit
 cp scripts/dot.preinit.bootserver /etc/puavo-conf/scripts/.preinit.bootserver
 
-cp scripts/setup_amxa_vars /etc/puavo-conf/scripts/.
+cp scripts/setup_extra_vars /etc/puavo-conf/scripts/.
 
 
 touch /etc/puavo-conf/puavo-conf.extra
 
-for PKG in $(cat packages.list) do
+for PKG in $(cat packages.list); do
     echo "puavo.pkg.$PKG"                >> /etc/puavo-conf/puavo-conf.extra 
 done
 
